9a89caa36f4d35a60c1e6442fa9aabc82856c0f0
zztang <y23301042@stu.ahu.edu.cn>
2025-08-22 10:50:12 +0800

    first update file

---
+++ network/DDL_Source_3Layer.py
@@ -0,0 +1,149 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchinfo import summary
+from thop import profile
+
+class DWConv(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=1):
+        super(DWConv, self).__init__()
+        self.depthwise = nn.Conv2d(in_channels, in_channels, kernel_size=kernel_size, stride=stride, padding=padding, groups=in_channels)
+        self.pointwise = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+        
+    def forward(self, x):
+        x = self.depthwise(x)
+        x = self.pointwise(x)
+        return x
+
+# LKA变形卷积，输入的不是3维图像也可以尝试使用
+class LKA(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        # 深度卷积
+        self.conv0 = nn.Conv2d(dim, dim, 5, padding=2, groups=dim)
+        # 深度空洞卷积
+        self.conv_spatial = nn.Conv2d(dim, dim, 7, stride=1, padding=9, groups=dim, dilation=3)
+        # 逐点卷积
+        self.conv1 = nn.Conv2d(dim, dim, 1)
+
+    def forward(self, x):
+        u = x.clone()
+        attn = self.conv0(x)
+        attn = self.conv_spatial(attn)
+        attn = self.conv1(attn)
+
+        # 注意力操作
+        return u * attn
+
+class DoubleConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+
+        self.dwconv1 = DWConv(in_channels=in_channels,out_channels=out_channels,kernel_size=3,padding=1)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+
+        self.dwconv2 = DWConv(in_channels=out_channels,out_channels=out_channels,kernel_size=3,padding=1)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
+        self.lka = LKA(out_channels)
+
+    def forward(self, x):
+        x = self.dwconv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+
+        x = self.dwconv2(x)
+        x = self.bn2(x)
+        x = self.relu(x)
+
+        x = self.lka(x)
+
+        return x
+
+class Down(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.maxpool_conv = nn.Sequential(
+            nn.MaxPool2d(2),
+            DoubleConv(in_channels=in_channels, out_channels=out_channels)
+        )
+
+    def forward(self, x):
+        return self.maxpool_conv(x)
+
+class Up(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.up = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2)
+        self.conv = DoubleConv(in_channels=out_channels * 2, out_channels=out_channels)
+
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+
+        # 适配尺寸
+        diffY = x2.size()[2] - x1.size()[2]
+        diffX = x2.size()[3] - x1.size()[3]
+
+        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
+                        diffY // 2, diffY - diffY // 2])
+        
+        # 合并上采样的特征图和跳跃连接的特征图
+        x = torch.cat([x2, x1], dim=1)
+        return self.conv(x)
+
+
+class OutConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(OutConv, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+
+    def forward(self, x):
+        return self.conv(x)
+
+class UNet(nn.Module):
+    def __init__(self, n_channels, n_classes, use_LKA=True):
+        super(UNet, self).__init__()
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+
+        self.inc = DoubleConv(n_channels, 96)
+        self.down1 = Down(96, 192)
+        self.down2 = Down(192, 384)
+        self.down3 = Down(384, 768)  # 删除了原来的down4层
+
+        self.up1 = Up(768, 384)      # 调整上采样层的输入通道数
+        self.up2 = Up(384, 192)      # 384 (来自up1) + 192 (来自down2)
+        self.up3 = Up(192, 96)       # 192 (来自up2) + 96 (来自down1)
+
+        self.outc = OutConv(96, n_classes)
+
+    def forward(self, x):
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x = self.up1(x4, x3)
+        x = self.up2(x, x2)
+        x = self.up3(x, x1)
+        
+        logits = self.outc(x)
+        return logits
+
+if __name__ == "__main__":
+
+    # 检查加速功能是否可用
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+        print("Using GPU:", torch.cuda.get_device_name(0))
+    else:
+        device = torch.device("cpu")
+        print("Using CPU")
+
+    # 初始化模型并将其移动到 GPU（如果可用）
+    model = UNet(n_channels=1, n_classes=9, use_LKA=False).to(device)
+    input_tensor = torch.randn(3, 1, 224, 224).to(device)
+    print("Input shape:", input_tensor.shape)
+    output = model(input_tensor)
+    print("Output shape:", output.shape)
+    summary(model=model,input_size=(1,1,224,224))
